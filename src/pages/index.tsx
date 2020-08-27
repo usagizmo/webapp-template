01cf40a0286140875b58be8171b28c27576b695a
usagizmo <usagizmo@gmail.com>
2020-08-27 20:31:54 +0900

    Organize meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,13 @@ const Home = (): JSX.Element => (
   <div className="container">
     <Head>
       <title>Nextjs Template</title>
-      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Nextjs Template" />
+      <meta property="og:description" content="Nextjs Template" />
+      <meta property="og:url" content="https://nextjs-template.io" />
+      <meta property="og:image" content="https://nextjs-template.io/ogp.png" />
+      <meta name="twitter:title" content="Nextjs Template" />
+      <meta name="twitter:description" content="Nextjs Template" />
     </Head>
 
     <main>
