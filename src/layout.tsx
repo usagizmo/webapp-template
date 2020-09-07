1481f61f8e3145e04f722217dc763f91b5fcb13e
usagizmo <usagizmo@gmail.com>
2020-09-07 21:36:29 +0900

    Update some plugins

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react'
 import Head from 'next/head'
 import { Router } from 'next/router'
-import useScrollYEmitter from '../plugins/use-scroll-y-emitter'
+
+// import useTypekit from '../plugins/typekit/use-typekit'
+import useScrollYEmitter from '../plugins/scroll/use-scroll-y-emitter'
 import useWindowSizeProvider from '../plugins/window-size/use-window-size-provider'
+// import PGtag from '../plugins/p-gtag'
+import useBodyProvider from '../plugins/use-body-provider'
 
 interface Props {
   router: Router
@@ -12,6 +16,7 @@ const Layout: FC<Props> = ({ children }) => {
   // useTypekit('abcdefg')
   useScrollYEmitter()
   useWindowSizeProvider()
+  useBodyProvider()
 
   return (
     <>
@@ -20,6 +25,7 @@ const Layout: FC<Props> = ({ children }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@nextjs-template" />
         <link rel="icon" href="/favicon.ico" />
+        {/*<PGtag uid="UA-12345678-9" />*/}
       </Head>
       {children}
     </>
