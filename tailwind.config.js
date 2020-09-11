ace14b01636e090334015ddca65bef3830bf65c6
usagizmo <usagizmo@gmail.com>
2020-09-11 10:26:05 +0900

    Add theme comments to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,34 +29,40 @@ module.exports = {
       },
     },
     extend: {
-      // fontFamily: {
-      //   base:
-      //     '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Helvetica Neue", HelveticaNeue, YuGothic, "Yu Gothic Medium", "Yu Gothic", Verdana, Meiryo, sans-serif',
-      // },
-      // fontSize: {
-      //   '10px': '0.625rem',
-      //   '11px': '0.6875rem',
-      //   '12px': '0.75rem',  // xs
-      //   '13px': '0.8125rem',
-      //   '14px': '0.875rem',  // sm
-      //   '16px': '1rem', // base
-      //   '18px': '1.125rem', // lg
-      //   '20px': '1.25rem', // xl
-      //   '24px': '1.5rem', // 2xl
-      //   '26px': '1.625rem',
-      //   '30px': '1.875rem', // 3xl
-      //   '36px': '2.25rem', // 4xl
-      //   '48px': '3rem', // 5xl
-      //   '64px': '4rem', // 6xl
-      // },
-      // spacing: {
-      //   '6px': '0.375rem',
-      // },
+      fontFamily: {
+        // base:
+        //   '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Helvetica Neue", HelveticaNeue, YuGothic, "Yu Gothic Medium", "Yu Gothic", Verdana, Meiryo, sans-serif',
+        // hiragino:
+        //   '"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif',
+      },
+      fontSize: {
+        '10px': '0.625rem',
+        '11px': '0.6875rem',
+        // '12px': '0.75rem', // xs
+        '13px': '0.8125rem',
+        // '14px': '0.875rem', // sm
+        '15px': '0.9375rem',
+        // '16px': '1rem', // base
+        // '18px': '1.125rem', // lg
+        // '20px': '1.25rem', // xl
+        '22px': '1.375rem',
+        // '24px': '1.5rem', // 2xl
+        '26px': '1.625rem',
+        '28px': '1.75rem',
+        // '30px': '1.875rem', // 3xl
+        '32px': '2rem',
+        '34px': '2.125rem',
+        // '36px': '2.25rem', // 4xl
+        // '48px': '3rem', // 5xl
+        // '64px': '4rem', // 6xl
+      },
+      spacing: {
+        em: '1em',
+        // '6px': '0.375rem',
+        // '100': '25rem',
+      },
       // inset: {
       //   '1/2': '50%',
-      // },
-      // opacity: {
-      //   '80': '0.8',
       // },
     },
   },
