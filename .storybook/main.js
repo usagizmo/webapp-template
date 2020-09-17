8c755774c5de301d8aee4a2fb718b2095221f4a1
usagizmo <usagizmo@gmail.com>
2020-09-17 09:32:36 +0900

    Change CSS plugins to stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../mock/**/*.stories.mdx',
+    '../mock/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
