9d536e84b15af3b63ad7f2769d5cd3610c4c8ad7
usagizmo <usagizmo@gmail.com>
2020-09-18 08:32:51 +0900

    Clean up the structure
    - Remove CSS plugin structure
    - Remove mock stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../mock/**/*.stories.mdx',
-    '../mock/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
