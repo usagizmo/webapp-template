1bda21eaae49fdd998a392578128a16c70278069
usagizmo <usagizmo@gmail.com>
2020-10-14 20:36:17 +0900

    Create 404 page (#23)

---
+++ app/src/pages/404.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+interface Props {}
+
+const _404Page: NextPage<Props> = () => {
+  return (
+    <>
+      <Head>
+        <title>404 Not Found - Next.js Template</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <main className="flex items-center justify-center h-screen">
+        <h1 className="flex items-center h-16 pr-6 mr-6 border-r border-gray-500 text-2xl font-medium">
+          404
+        </h1>
+        <p className="text-sm">This page could not be found.</p>
+      </main>
+    </>
+  )
+}
+
+export default _404Page
