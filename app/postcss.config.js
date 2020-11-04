662e58f9696bcf2105e21c03cd807100bf195bb7
usagizmo <usagizmo@gmail.com>
2020-11-04 10:42:52 +0900

    Add postcss-flexbugs-fixes

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     tailwindcss: {},
+    'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
       browsers: 'last 2 versions',
       stage: 3,
