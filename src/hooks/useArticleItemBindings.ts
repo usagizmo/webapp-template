1b09789e613a794b93531f89d4ea1eb0e3e32223
usagizmo <usagizmo@gmail.com>
2021-11-07 04:58:17 +0900

    Remove unnecessary update

--- src/hooks/useArticleItemBindings.ts
+++ src/hooks/useArticleItemBindings.ts
@@ -17,6 +17,7 @@ export const useArticleItemBindings = (article: Article) => {
       setTitle(e.target.value)
     },
     onBlur: () => {
+      if (article.title === title) return
       updateArticleMutation.mutate({
         ...article,
         title,
@@ -32,6 +33,7 @@ export const useArticleItemBindings = (article: Article) => {
       setContent(e.target.value)
     },
     onBlur: () => {
+      if (article.content === content) return
       updateArticleMutation.mutate({
         ...article,
         content,
