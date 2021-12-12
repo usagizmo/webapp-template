8eb31c68f1f792c943ca09cf7c8eb89dace409a7
usagizmo <usagizmo@gmail.com>
2021-12-12 12:21:53 +0900

    Change site images

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,13 +24,13 @@ const Layout: VFC<Props> = ({
         <meta property="og:title" content={pageTitle} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://nextjs-template.io/" />
-        <meta property="og:image" content="https://nextjs-template.io/images/ogp-fb.png" />
+        <meta property="og:image" content="https://nextjs-template.io/images/ogp.jpg" />
         <meta property="og:site_name" content={CONST.SITE_NAME} />
         <meta property="og:description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="https://nextjs-template.io/images/ogp-tw.png" />
+        <meta name="twitter:image" content="https://nextjs-template.io/images/ogp.jpg" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         {/* <link rel="canonical" href="https://nextjs-template.io/" /> */}
