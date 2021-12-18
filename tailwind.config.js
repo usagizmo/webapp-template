2f3ed2474697a5946f93b9d76d301f2ca2bebfee
usagizmo <usagizmo@gmail.com>
2021-12-19 01:03:06 +0900

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '24px',
+    },
     fontFamily: {
       // When not using Web fonts
       // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Yu Gothic", YuGothic, "BIZ UDPGothic", Meiryo, sans-serif',
@@ -13,28 +13,6 @@ module.exports = {
       mono: ['Source Code Pro', 'Noto Sans CJK JP', 'Noto Sans JP', 'monospace'],
       keycode: ['Lucida Grande'],
     },
-    container: {
-      center: true,
-      padding: '24px',
-    },
-    extend: {
-      colors: {
-        // https://tailwindcss.com/docs/customizing-colors
-        orange: colors.orange,
-        amber: colors.amber,
-        lime: colors.lime,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        sky: colors.sky,
-        violet: colors.violet,
-        fuchsia: colors.fuchsia,
-        rose: colors.rose,
-      },
-    },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
