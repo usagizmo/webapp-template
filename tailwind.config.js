bc1136ab2cd37f361dd6ea716e454290b7a4fc3b
usagizmo <usagizmo@gmail.com>
2021-12-20 01:55:17 +0900

    Move fontFamily settings in tailwind.config.js to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,15 @@ module.exports = {
       center: true,
       padding: '24px',
     },
-    fontFamily: {
-      // When not using Web fonts
-      // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Yu Gothic", YuGothic, "BIZ UDPGothic", Meiryo, sans-serif',
-      // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Hiragino Sans", "Hiragino Kaku Gothic ProN", "BIZ UDPGothic", Meiryo, sans-serif',
-      sans: ['Inter', 'yakuhanjp_Noto', 'Noto Sans CJK JP', 'Noto Sans JP', 'sans-serif'],
-      mono: ['Source Code Pro', 'Noto Sans CJK JP', 'Noto Sans JP', 'monospace'],
-      keycode: ['Lucida Grande'],
+    extend: {
+      fontFamily: {
+        // When not using Web fonts
+        // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Yu Gothic", YuGothic, "BIZ UDPGothic", Meiryo, sans-serif',
+        // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Hiragino Sans", "Hiragino Kaku Gothic ProN", "BIZ UDPGothic", Meiryo, sans-serif',
+        sans: ['Inter', 'yakuhanjp_Noto', 'Noto Sans CJK JP', 'Noto Sans JP', 'sans-serif'],
+        mono: ['Source Code Pro', 'Noto Sans CJK JP', 'Noto Sans JP', 'monospace'],
+        keycode: ['Lucida Grande'],
+      },
     },
   },
   plugins: [],
