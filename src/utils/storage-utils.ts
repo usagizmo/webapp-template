e48f3200e5676ae37d0b0d97a03dfd4fefdb19b2
usagizmo <usagizmo@gmail.com>
2022-02-13 18:58:23 +0900

    Update import order

--- src/utils/storage-utils.ts
+++ src/utils/storage-utils.ts
@@ -1,5 +1,5 @@
-import { storage } from '@/libs/firebase'
 import { ref } from 'firebase/storage'
 import { ulid } from 'ulid'
+import { storage } from '@/libs/firebase'
 
 export const getArticlesStorageRef = () => ref(storage, `articles/${ulid()}`)
