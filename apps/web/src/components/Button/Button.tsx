95a12e6fa039d420f426381feca30a6089f05e92
usagizmo <usagizmo@gmail.com>
2022-04-09 21:59:41 +0900

    Button component `as span` support

--- apps/web/src/components/Button/Button.tsx
+++ apps/web/src/components/Button/Button.tsx
@@ -1,14 +1,22 @@
-import { MouseEventHandler, FC } from 'react'
+import { MouseEventHandler, FC, useMemo } from 'react'
+import { XOR } from 'ts-xor'
 import { classNames } from 'utils'
 
-type Props = {
+type ButtonProps = {
   type?: 'submit' | 'button' | 'reset'
+  onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+type SpanProps = {
+  as: 'span'
+}
+
+type Props = {
   primary?: boolean
   black?: boolean
   icon?: boolean
   disabled?: boolean
-  onClick?: MouseEventHandler<HTMLButtonElement>
-}
+} & XOR<ButtonProps, SpanProps>
 
 export const Button: FC<Props> = ({
   type = 'button',
@@ -17,15 +25,14 @@ export const Button: FC<Props> = ({
   icon,
   disabled,
   onClick,
+  as,
   children,
 }) => {
   const isDefaultColor = !primary && !black
-
-  return (
-    <button
-      type={type}
-      className={classNames(
-        'inline-flex items-center justify-center rounded-full px-5 py-2.5 text-center text-sm font-medium focus:outline-none focus:ring-4 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+  const buttonStyle = useMemo(
+    () =>
+      classNames(
+        'inline-flex items-center justify-center rounded-full px-5 py-2.5 text-center text-sm font-medium focus:outline-none focus:ring-4',
         primary
           ? 'bg-red-700 text-white hover:bg-red-800 focus:ring-red-300'
           : '',
@@ -35,8 +42,20 @@ export const Button: FC<Props> = ({
         isDefaultColor
           ? 'bg-gray-100 hover:bg-gray-200 focus:ring-gray-300'
           : '',
+        disabled
+          ? 'pointer-events-none disabled:cursor-not-allowed disabled:opacity-50'
+          : '',
         icon ? 'h-6 w-6 !p-0' : ''
-      )}
+      ),
+    [primary, black, isDefaultColor, disabled, icon]
+  )
+
+  return as === 'span' ? (
+    <span className={buttonStyle}>{children}</span>
+  ) : (
+    <button
+      type={type}
+      className={buttonStyle}
       onClick={onClick}
       disabled={disabled}
     >
