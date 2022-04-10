d432f3c7cd0db64d2163191e2c8039517953d575
usagizmo <usagizmo@gmail.com>
2022-04-10 13:04:28 +0900

    Add description to constants/const

--- apps/web/src/pages/_app/components/Layout/Layout.tsx
+++ apps/web/src/pages/_app/components/Layout/Layout.tsx
@@ -19,7 +19,7 @@ type Props = {}
 
 const useLayout = () => {
   const title = CONST.SITE_NAME
-  const description = 'Next.js Template Description'
+  const description = CONST.DESCRIPTION
 
   useSmoothScroll()
 
