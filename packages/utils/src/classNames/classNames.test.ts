b0a93181ff314053b99b16f05768bf931b672916
usagizmo <usagizmo@gmail.com>
2022-04-19 20:44:11 +0900

    Use clsx and ts-pattern

--- packages/utils/src/classNames/classNames.test.ts
+++
@@ -1,5 +0,0 @@
-import { classNames } from './classNames'
-
-test('Class names are properly added', () => {
-  expect(classNames('piyo piyo2 piyo3', 'piyo4')).toBe('piyo piyo2 piyo3 piyo4')
-})
--- packages/utils/src/classNames/classNames.ts
+++
@@ -1,2 +0,0 @@
-export const classNames = (...classes: string[]): string =>
-  classes.filter(Boolean).join(' ')
