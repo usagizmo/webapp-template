b0d69ad611ee40fcadc95182fad8f6a9a8fba0c5
usagizmo <usagizmo@gmail.com>
2022-05-22 07:10:16 +0900

    Update/Fix packages (#326)
    * Fix package/utils linting by tsconfig/react-library
    * Fix packages/types

--- apps/web/src/types.d.ts
+++ apps/web/src/types.d.ts
@@ -1,1 +1,9 @@
-import 'types'
+import type { CurrentUserQuery } from 'generated/dist/graphql-api'
+import type { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  interface Session {
+    idToken: string
+    user: DefaultSession['user'] & NonNullable<CurrentUserQuery['users_by_pk']>
+  }
+}
--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,1 +1,3 @@
-import './src/next-auth'
+export type * from './src/data-types'
+export type * from './src/extend-types.d.ts'
+export type * from './src/types.d.ts'
