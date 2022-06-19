0f8f3ff1fcc4cb476461d025530763f0b8890c0e
usagizmo <usagizmo@gmail.com>
2022-06-19 20:50:32 +0900

    Remove incorrect braces

--- packages/lintstagedrc/index.cjs
+++ packages/lintstagedrc/index.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{js,cjs,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{html}': ['htmlhint', 'prettier --write'],
+  '*.html': ['htmlhint', 'prettier --write'],
 }
