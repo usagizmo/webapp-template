4f0791cedcb3c79d263ada9f35435ebedcf81479
usagizmo <usagizmo@gmail.com>
2022-12-08 04:48:01 +0900

    feat: add simple validation

--- apps/web/src/routes/admin/(isNotLoggedIn)/AdminForm.svelte
+++ apps/web/src/routes/admin/(isNotLoggedIn)/AdminForm.svelte
@@ -22,15 +22,20 @@
         label="Display Name"
         type="text"
         bind:value={inputs.displayName}
-        error="Display Name is required."
+        error={{ required: 'Display Name is required.' }}
       />
     </div>
   {/if}
-  <Input label="Email" type="email" bind:value={inputs.email} error="E-mail is required." />
+  <Input
+    label="Email"
+    type="email"
+    bind:value={inputs.email}
+    error={{ required: 'E-mail is required.' }}
+  />
   <Input
     label="Password"
     type="password"
     bind:value={inputs.password}
-    error="Password is required."
+    error={{ required: 'Password is required.' }}
   />
 </div>
--- packages/ui/components/Input.svelte
+++ packages/ui/components/Input.svelte
@@ -2,23 +2,26 @@
   export let label = '';
   export let type = 'text';
   export let value = '';
-  export let error = '';
+  export let error: { required?: string } = {};
 
-  const typeAction = (node: HTMLInputElement) => {
-    node.type = type;
+  let classes = 'w-full rounded-md border border-zinc-300 bg-slate-50 py-2 px-2.5';
+  let isDirty = false;
+
+  const handleDirty = () => {
+    isDirty = true;
   };
 </script>
 
 <div>
   <label>
     <span class="mb-1 block font-semibold">{label}</span>
-    <input
-      use:typeAction
-      bind:value
-      class="w-full rounded-md border border-zinc-300 bg-slate-50 py-2 px-2.5"
-    />
+    {#if type === 'password'}
+      <input type="password" class={classes} bind:value on:blur={handleDirty} />
+    {:else}
+      <input type="text" class={classes} bind:value on:blur={handleDirty} />
+    {/if}
   </label>
-  {#if error}
-    <div class="mt-1 text-sm text-red-600">{error}</div>
+  {#if error.required && isDirty && !value}
+    <div class="mt-1 text-sm text-red-600">{error.required}</div>
   {/if}
 </div>
