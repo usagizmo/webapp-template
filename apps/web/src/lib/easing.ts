3deb0f4b144a3988d37d12cdabc85f2f36df9d3d
usagizmo <usagizmo@gmail.com>
2022-12-20 15:46:38 +0900

    fix: use `as const satisfies` when using `export const` (#677)

--- apps/web/src/lib/easing.ts
+++ apps/web/src/lib/easing.ts
@@ -3,4 +3,7 @@ import { quintOut } from 'svelte/easing';
 export const defaultDE = {
   duration: 450,
   easing: quintOut,
+} as const satisfies {
+  duration: number;
+  easing: (t: number) => number;
 };
--- apps/web/src/lib/store.ts
+++ apps/web/src/lib/store.ts
@@ -1,8 +1,8 @@
 import { writable } from 'svelte/store';
 import { isLoggedIn } from './nhost';
-import { ROUTE } from './routes';
+import { ROUTE, type ROUTE_VALUE } from './routes';
 
-export const adminPath = writable(ROUTE.ADMIN);
+export const adminPath = writable<ROUTE_VALUE>(ROUTE.ADMIN);
 
 isLoggedIn.subscribe((value) => {
   adminPath.set(value ? ROUTE.ADMIN : ROUTE.ADMIN_LOGIN);
