3875a2ea9bf8ef009086e03dfb6446418963e511
usagizmo <usagizmo@gmail.com>
2022-12-20 15:14:11 +0900

    fix: use `as const satisfies` when using `export const`

--- apps/web/src/lib/routes.ts
+++ apps/web/src/lib/routes.ts
@@ -4,4 +4,8 @@ export const ROUTE = {
   ADMIN_LOGIN: '/admin/login',
   ADMIN_SIGNUP: '/admin/signup',
   SECRET: '/secret',
+} as const satisfies {
+  [key in string]: `/${string}`;
 };
+
+export type ROUTE_VALUE = typeof ROUTE[keyof typeof ROUTE];
