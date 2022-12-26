91f1b9c31205391ec3add73bfbf7967bf7c9ed33
usagizmo <usagizmo@gmail.com>
2022-12-26 15:42:03 +0900

    fix: #each transition

--- apps/web/src/routes/Comments.svelte
+++ apps/web/src/routes/Comments.svelte
@@ -2,14 +2,17 @@
   import { fade } from 'svelte/transition';
   import { flip } from 'svelte/animate';
   import { defaultDE } from '$lib/easing';
-  import CommentItem from './Comment.svelte';
   import type { Comment } from '$houdini';
+  import CommentItem from './Comment.svelte';
+  import { toWithId } from '$lib/utils';
 
   export let comments: Comment[] = [];
+
+  $: commentsWithId = comments.map(toWithId);
 </script>
 
 <div class="divide-y divide-slate-200">
-  {#each comments as comment (comment)}
+  {#each commentsWithId as comment (comment.id)}
     <div transition:fade|local={defaultDE} animate:flip={defaultDE}>
       <CommentItem {comment} />
     </div>
