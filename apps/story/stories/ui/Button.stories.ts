106da3a8a7f87acc157124de5ab76a299a0d0c90
usagizmo <usagizmo@gmail.com>
2023-04-20 23:30:06 +0900

    feat: add eslint to apps/story (#746)

--- apps/story/stories/ui/Button.stories.ts
+++ apps/story/stories/ui/Button.stories.ts
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/svelte';
 import ButtonView from './views/ButtonView.svelte';
+import type { Meta, StoryObj } from '@storybook/svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
 const meta = {
