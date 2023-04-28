77d64cb858b9d62153026d7099a284f385f1b28b
usagizmo <usagizmo@gmail.com>
2023-04-28 22:50:04 +0900

    fix: apps/web component path on `story`

--- apps/story/tailwind.config.js
+++ apps/story/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   presets: [base],
   content: [
     './stories/**/*.svelte',
-    '../../apps/web/lib/**/*.svelte', // for using 'apps/web'
+    '../../apps/web/src/lib/**/*.svelte', // for using 'apps/web'
     '../../packages/ui/components/**/*.svelte', // for using 'packages/ui'
   ],
 };
