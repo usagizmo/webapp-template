63b6f377e43396c2d3d899e86e41b6bbd93ca80a
usagizmo <usagizmo@gmail.com>
2023-05-15 19:40:39 +0900

    💬 Update deployment message on mockup

--- apps/mockup/commands/deploy.sh
+++ apps/mockup/commands/deploy.sh
@@ -1,2 +1,2 @@
 rsync -ahvu --delete --exclude=".*" public/ <SSH_HOST>:/var/www/html/
-echo "\npublished: https://webapp-template-mockup.usagizmo.com/\n"
+echo "\n🚀 \x1b[32mhttps://webapp-template-mockup.usagizmo.com/\x1b[0m\n"
