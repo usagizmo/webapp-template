ef0254ece76fc4b2931859e2171a963319257d5b
usagizmo <usagizmo@gmail.com>
2023-10-27 23:46:51 +0900

    🐛 Fix .markuplint settings (#775)

--- .markuplintrc.cjs
+++ .markuplintrc.cjs
@@ -3,14 +3,39 @@ module.exports = {
     '\\.svelte$': '@markuplint/svelte-parser',
   },
   extends: ['markuplint:recommended'],
-  excludeFiles: [
-    './apps/web/src/app.html',
-    './apps/web/src/routes/Comments.svelte', // TODO: remove this
-  ],
   rules: {
     'character-reference': false,
     'ineffective-attr': false,
     'label-has-control': false,
     'require-accessible-name': false,
   },
+  excludeFiles: [
+    // TODO: Once the overrides option is fixed, remove these lines
+    // ref: https://github.com/markuplint/markuplint/issues/1119
+    './apps/web/src/app.html',
+    './apps/web/src/routes/CommentForm.svelte',
+  ],
+  nodeRules: [
+    // For Svelte
+    {
+      selector: 'textarea',
+      rules: {
+        'invalid-attr': {
+          options: {
+            allowAttrs: ['value'],
+          },
+        },
+      },
+    },
+    {
+      selector: 'input[type="file"]',
+      rules: {
+        'invalid-attr': {
+          options: {
+            allowAttrs: ['files'],
+          },
+        },
+      },
+    },
+  ],
 };
