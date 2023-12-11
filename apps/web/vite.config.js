d340f7adf79f3c987367c02dae984cc49c247994
usagizmo <usagizmo@gmail.com>
2023-12-12 04:08:24 +0900

    🚧 Added resolve to vite.config.js for using Vite 5

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -5,6 +5,11 @@ import houdini from 'houdini/vite';
 export default {
   plugins: [houdini(), sveltekit()],
   test: {
-      include: ['src/**/*.{test,spec}.{js,ts}'],
-    },
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
+  // TODO: To use Vite 5 for now
+  // ref. https://github.com/vitejs/vite/issues/15274
+  resolve: {
+    mainFields: ['browser', 'module', 'jsnext:main', 'jsnext'],
+  },
 };
