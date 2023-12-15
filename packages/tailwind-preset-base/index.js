8e54fa1fb14a1472ba1780280d94833ff18cc4a3
usagizmo <usagizmo@gmail.com>
2023-12-15 15:01:19 +0900

    ⬆️ Update YakuHanJP to v4

--- packages/tailwind-preset-base/index.js
+++ packages/tailwind-preset-base/index.js
@@ -7,7 +7,7 @@ export default {
         // When not using Web fonts
         // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Yu Gothic", YuGothic, "BIZ UDPGothic", Meiryo, sans-serif',
         // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Hiragino Sans", "Hiragino Kaku Gothic ProN", "BIZ UDPGothic", Meiryo, sans-serif',
-        sans: 'Inter, yakuhanjp_Noto, Noto Sans JP, sans-serif',
+        sans: 'Inter, YakuHanJP, Noto Sans JP, sans-serif',
         mono: 'Source Code Pro, Noto Sans JP, monospace',
         keycode: 'Lucida Grande',
       },
