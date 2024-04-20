4ce5875cfd6e5162380b8380d98bd41bdefb0bb3
usagizmo <usagizmo@gmail.com>
2024-04-20 22:28:34 +0900

    🔥 Remove `apps/nhost`

--- apps/nhost/nhost/migrations/default/1706038971180_create_table_public_profiles/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "public"."profiles" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "bio" text NOT NULL DEFAULT ''::text, PRIMARY KEY ("id") , FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON UPDATE restrict ON DELETE cascade);
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
