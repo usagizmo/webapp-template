cd606787e59051cdc5ac956a300093745ced2369
usagizmo <usagizmo@gmail.com>
2024-04-27 09:21:58 +0900

    🔧 Update build process and type generation for Supabase
    * Remove `SUPABASE_DIRECT_URL` environment variable from GitHub Actions build step
    * Change `.gitignore` to keep `supabase-types.ts` instead of `.gitkeep`
    * Refactor `supabase-types.js` to use dynamic path for output file
    * Remove `svelte-kit sync` from `generate` and `build` scripts in `package.json`
    * Modify `turbo.json` to remove dependency

--- apps/web/commands/supabase-types.js
+++ apps/web/commands/supabase-types.js
@@ -1,8 +1,13 @@
 import { config } from 'dotenv';
 import { execSync } from 'node:child_process';
+import * as path from 'node:path';
+import url from 'node:url';
 
 config();
 
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const outputPath = path.join(__dirname, '..', 'src', 'lib', '$generated', 'supabase-types.ts');
+
 execSync(
-  `supabase gen types typescript --db-url ${process.env.SUPABASE_DIRECT_URL} > ./src/lib/\\$generated/supabase-types.ts`,
+  `supabase gen types typescript --db-url ${process.env.SUPABASE_DIRECT_URL} > '${outputPath}'`,
 );
