9087fbe8da9fb1848e6bb05eb88832d66d9b616a
usagizmo <usagizmo@gmail.com>
2024-04-28 14:43:54 +0900

    🔧 Simplify linting configuration in .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 export default {
   '*': ['cspell --no-must-find-files', 'prettier --write --ignore-unknown'],
-  '*.html': ['markuplint', 'prettier --write'],
-  '*.css': ['prettier --write'],
-  '*.{js,cjs,mjs,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.svelte': ['markuplint', 'eslint --fix', 'prettier --write'],
+  '*.html': ['markuplint'],
+  '*.{js,cjs,mjs,jsx,ts,tsx}': ['eslint --fix'],
+  '*.svelte': ['markuplint', 'eslint --fix'],
 };
