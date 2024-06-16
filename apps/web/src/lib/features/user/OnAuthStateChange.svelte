4d4697b816f7bdab32618c0957a8db4ffd66db34
usagizmo <usagizmo@gmail.com>
2024-06-17 08:54:42 +0900

    ♻️ Refactor authentication state handling

---
+++ apps/web/src/lib/features/user/OnAuthStateChange.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { AuthSession } from '@supabase/supabase-js';
+  import { onMount } from 'svelte';
+
+  import { userStore } from '$lib/features/user/userStore.svelte';
+  import { getUser } from '$lib/features/user/userUtils';
+  import { supabase } from '$lib/supabase';
+
+  let session = $state<AuthSession | null>(null);
+
+  $effect(() => {
+    if (!session) {
+      userStore.user = null;
+      return;
+    }
+
+    getUser(session.user.id).then(({ user }) => {
+      userStore.user = user;
+    });
+  });
+
+  onMount(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      session = data.session;
+    });
+
+    supabase.auth.onAuthStateChange((_event, _session) => {
+      session = _session;
+    });
+  });
+</script>
