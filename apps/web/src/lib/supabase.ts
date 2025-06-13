02206b54a38efe5592e4719018954274d76a74f9
usagizmo <usagizmo@gmail.com>
2025-06-13 16:43:25 +0900

    ✨ Improve type definition generation process

--- apps/web/src/lib/supabase.ts
+++ apps/web/src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
-import type { Database } from './$generated/supabase-types';
+import type { Database } from '@repo/types/supabase';
 
 export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
