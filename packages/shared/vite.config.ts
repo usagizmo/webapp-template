6430475df9831464a58ec910ec3dff2634ead8f6
usagizmo <usagizmo@gmail.com>
2025-07-26 21:55:28 +0900

    🔧 Fix Vercel build error by adding .svelte-kit to turbo outputs and remove unnecessary $lib alias

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -1,12 +1,6 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [svelte()],
-  resolve: {
-    alias: {
-      $lib: resolve(__dirname, './src/lib'),
-    },
-  },
 });
