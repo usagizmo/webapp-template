b4872584621218380c2e28478a8545cc43fce255
usagizmo <usagizmo@gmail.com>
2025-08-19 14:22:50 +0900

    🔧 Fix deploy script to use absolute path for public directory

--- apps/pages/commands/deploy.sh
+++ apps/pages/commands/deploy.sh
@@ -1,2 +1,3 @@
-rsync -ahvu --delete --exclude=".*" public/ <SSH_HOST>:/var/www/html/
+PKG_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+rsync -ahvu --delete --exclude=".*" $PKG_DIR/public/ <SSH_HOST>:/var/www/html/
 echo "\n🚀 \x1b[32mhttps://webapp-template-pages.usagizmo.com/\x1b[0m\n"
