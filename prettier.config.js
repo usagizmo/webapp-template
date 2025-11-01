bf5f79279bc45088625ba33db41ac4b71e82cf4a
usagizmo <usagizmo@gmail.com>
2025-11-01 16:01:44 +0900

    🔧 Optimize Prettier and turbo.json settings

--- prettier.config.js
+++ prettier.config.js
@@ -4,13 +4,7 @@ const config = {
   printWidth: 100,
   plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
   overrides: [
-    {
-      files: '*.svelte',
-      options: {
-        parser: 'svelte',
-        plugins: ['prettier-plugin-svelte'],
-      },
-    },
+    { files: '*.svelte', options: { parser: 'svelte', plugins: ['prettier-plugin-svelte'] } },
   ],
   tailwindFunctions: ['$derived', 'tv'],
 };
