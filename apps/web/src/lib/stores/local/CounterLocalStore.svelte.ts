15de7bb85cb5e22d16674acbd5e138f4484d9d4f
usagizmo <usagizmo@gmail.com>
2025-11-07 04:00:06 +0900

    🚚 Rename LocalCounter to CounterLocalStore

--- apps/web/src/lib/stores/local/CounterLocalStore.svelte.ts
+++ apps/web/src/lib/stores/local/CounterLocalStore.svelte.ts
@@ -1,8 +1,8 @@
 /**
  * Counter LocalStore (component-scoped)
- * Usage: `const counter = new LocalCounter()`
+ * Usage: `const counter = new CounterLocalStore()`
  */
-export class LocalCounter {
+export class CounterLocalStore {
   #count = $state(0);
 
   readonly count = $derived<number>(this.#count);
